f7f4b3b054d42cfd1b60dd7059ca21ea3361a3f7
Deividtecnologia <00001131449836SP@al.educacao.sp.gov.br>
2024-11-04 11:03:26 -0300

    Pão

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é scratch?',
+    'O scratch é uma plataforma de programação em blocos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da italia ?',
+    'A capital da italia é roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um codigo',
+    'Um processo digital de codificação de sinais'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz feliz em ingles?',
+    'feliz em ingles se diz happy.'
+)
